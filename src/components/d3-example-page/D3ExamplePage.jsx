4ff3a82962633922186e4793780d2efae38117ec
ccavel <ccavel@ensc.fr>
2020-10-16 23:26:09 +0200

    pb avec une varibale globale (qui ne veut pas l'être)
    Créations de fct pour attribuer une valeur y à chaque cue en fonction de leur subdomain --> pour cela création de 3 cues en dur

--- src/components/d3-example-page/D3ExamplePage.jsx
+++ src/components/d3-example-page/D3ExamplePage.jsx
@@ -2,9 +2,141 @@ import React, { useEffect, useRef } from 'react';
 import Chart from 'chart.js';
 
 import './D3ExamplePage.css';
+import { string } from 'prop-types';
+
+var cue1 = {
+    action : [{
+        id:"1",
+        startDate: "2020-10-02T15:16:42.427Z",
+        }],
+    description: "desc1",
+    iconType: "string",
+    id:"string",
+    isRepettitive : true,
+    isSuperCue: true,
+    triggerIds : ["1"],
+    name: "cue n°1",
+    number:0,
+    occurences : [{
+        minedDocumentId : "string",
+        miningState: true,
+        sourceQuality: 0,
+        computationTime: "2020-10-02T15:16:42.427Z",
+        lat:0,
+        lon:0,
+        cueEventId: "string"
+    }],
+    lat:0,
+    lon:0,
+    icon: "string",
+    subdomain: "subdomain1",
+    warningCriteriaDayRed : "COM",
+    warningCriteriaNightRed : "COM",
+    weighting: 5,
+    emergenceOriginId:"string"
+}
+
+var cue2 = {
+    action : [{
+        id:"2",
+        startDate: "2020-10-03T15:11:12.727Z",
+        }],
+    description: "desc2",
+    iconType: "string",
+    id:"string",
+    isRepettitive : true,
+    isSuperCue: true,
+    triggerIds : ["2"],
+    name: "cue n°2",
+    number:0,
+    occurences : [{
+        minedDocumentId : "string",
+        miningState: true,
+        sourceQuality: 0,
+        computationTime: "2020-10-03T15:11:12.727Z",
+        lat:0,
+        lon:0,
+        cueEventId: "string"
+    }],
+    lat:0,
+    lon:0,
+    icon: "string",
+    subdomain: "subdomain1",
+    warningCriteriaDayRed : "COM",
+    warningCriteriaNightRed : "COM",
+    weighting: 5,
+    emergenceOriginId:"string"
+}
+var cue3 = {
+    action : [{
+        id:"3",
+        startDate: "2020-10-05T15:21:34.122Z",
+        }],
+    description: "desc3",
+    iconType: "string",
+    id:"string",
+    isRepettitive : true,
+    isSuperCue: true,
+    triggerIds : ["3"],
+    name: "cue n°3",
+    number:0,
+    occurences : [{
+        minedDocumentId : "string",
+        miningState: true,
+        sourceQuality: 0,
+        computationTime: "2020-10-05T15:21:34.122Z",
+        lat:0,
+        lon:0,
+        cueEventId: "string"
+    }],
+    lat:0,
+    lon:0,
+    icon: "string",
+    subdomain: "subdomain2",
+    warningCriteriaDayRed : "COM",
+    warningCriteriaNightRed : "COM",
+    weighting: 5,
+    emergenceOriginId:"string"
+}
+var tabCues = [cue1, cue2, cue3]; //tableau de cues qui devrait être directement importé
+
+function setYValue() {
+    var tabSubdomains = ["subdomain1", "subdomain2", "subdomain3"];
+    var tabY = [];
+    for (let  i =0; i<this.tabCues.length; i++)
+    {
+        for (let  j =0; j<tabSubdomains.length; j++)
+        {
+            if (this.tabCues[i].subdomain == tabSubdomains[j])
+            {
+                tabY.push(j);
+            }
+        }
+    }
+    return tabY;
+}
+
+function setXYRValue() {
+    var tabY = setYValue();
+    var tabCueXYR= [];
+    var compteur = 0;
+    var x, y, r;
+    for (var oneCue in this.tabCues)
+    {
+        x=parseInt(oneCue.startDate.substring(8,9));
+        r=oneCue.weighting;
+        y=tabY[compteur];
+        tabCueXYR.push({x, y, r});
+        compteur++;
+    }
+    return tabCueXYR;
+}
+
+
 
 const ChartJsExamplePage = () => {
     const canvasRef = useRef();
+    const tabCueXYR = setXYRValue();
 
     useEffect(() => {      
         // For a bubble chart
@@ -15,6 +147,11 @@ const ChartJsExamplePage = () => {
                 datasets: [{
                     label: 'label1',
                     backgroundColor: 'rgba(255,0,0,0.6)',
+                    data: tabCueXYR
+                },
+                {
+                    label: 'label2',
+                    backgroundColor: 'rgba(0,255,0,0.6)',
                     data: [{
                         x: 1, 
                         y: 1,
@@ -25,7 +162,8 @@ const ChartJsExamplePage = () => {
                         y: 1,
                         r: 2,
                     }]
-                }],
+                },
+            ],
             },
             options: Chart.defaults.bubble,
         });
