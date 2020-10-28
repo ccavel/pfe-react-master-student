76a10a3fafdc1e9ed5d237f36422c19bf6fa9fdc
ccavel <ccavel@ensc.fr>
2020-10-28 20:27:24 +0100

    fct parcours des subdomains

--- src/components/d3-example-page/D3ExamplePage.jsx
+++ src/components/d3-example-page/D3ExamplePage.jsx
@@ -29,13 +29,12 @@ var cue1 = {
     lat:0,
     lon:0,
     icon: "string",
-    subdomain: "subdomain1",
+    subdomain: "Political",
     warningCriteriaDayRed : "COM",
     warningCriteriaNightRed : "COM",
     weighting: 5,
     emergenceOriginId:"string"
 }
-
 var cue2 = {
     action : [{
         id:"2",
@@ -61,10 +60,10 @@ var cue2 = {
     lat:0,
     lon:0,
     icon: "string",
-    subdomain: "subdomain1",
+    subdomain: "Political",
     warningCriteriaDayRed : "COM",
     warningCriteriaNightRed : "COM",
-    weighting: 5,
+    weighting: 15,
     emergenceOriginId:"string"
 }
 var cue3 = {
@@ -92,18 +91,70 @@ var cue3 = {
     lat:0,
     lon:0,
     icon: "string",
-    subdomain: "subdomain2",
+    subdomain: "Cyber",
     warningCriteriaDayRed : "COM",
     warningCriteriaNightRed : "COM",
-    weighting: 5,
+    weighting: 25,
     emergenceOriginId:"string"
 }
 
-var tabCues = [cue1, cue2, cue3];
+var cue4 = {
+    action : [{
+        id:"3",
+        startDate: "2020-10-05T15:21:34.122Z",
+        }],
+    description: "desc3",
+    iconType: "string",
+    id:"string",
+    isRepettitive : true,
+    isSuperCue: true,
+    triggerIds : ["3"],
+    name: "cue n°3",
+    number:0,
+    occurences : [{
+        minedDocumentId : "string",
+        miningState: true,
+        sourceQuality: 0,
+        computationTime: "2020-10-05T15:21:34.122Z",
+        lat:0,
+        lon:0,
+        cueEventId: "string"
+    }],
+    lat:0,
+    lon:0,
+    icon: "string",
+    subdomain: "Air",
+    warningCriteriaDayRed : "COM",
+    warningCriteriaNightRed : "COM",
+    weighting: 17,
+    emergenceOriginId:"string"
+}
+var tabCues = [cue1, cue2, cue3, cue4];
+
+function selectAllSubdomain() {
+    var tabSubdomains = [];
+    var double = false;
+    for (let  i =0; i<tabCues.length; i++)
+    {
+        for (let  j =0; j<tabSubdomains.length; j++)
+        {
+            if (tabCues[i].subdomain === tabSubdomains[j])
+            {
+                double = true;
+            }
+
+        } 
+        if (double === false)
+        {
+            tabSubdomains.push(tabCues[i].subdomain)
+        }  
+    }
+    console.log("tab subdomain : "+tabSubdomains);
+    return tabSubdomains;
+}
 
 function setYValue() {
-    console.log("test1 ");
-    var tabSubdomains = ["subdomain1", "subdomain2", "subdomain3"];
+    var tabSubdomains = selectAllSubdomain();
     var tabY = [];
     for (let  i =0; i<tabCues.length; i++)
     {
@@ -119,7 +170,7 @@ function setYValue() {
 }
 
 function setXYRValue() {
-    console.log("test XYRfunction " + tabCues[0].action[0].startDate);
+    console.debug("setXYRvalue");
     var tabY = setYValue();
     var tabCueXYR= [];
     var compteur = 0;
@@ -127,11 +178,12 @@ function setXYRValue() {
     for (let  i =0; i<tabCues.length; i++)
     {
         console.log("substring : " + tabCues[i].action[0].startDate.substring(8,10));
-        console.log ("x =" + parseInt(tabCues[i].action[0].startDate.substring(8,10)));
         x=parseInt(tabCues[i].action[0].startDate.substring(8,10));
-
         r=tabCues[i].weighting;
         y=tabY[compteur];
+        console.log ("x = " + x);
+        console.log ("y = " + y);
+        console.log ("r = " + r);
         tabCueXYR.push({x, y, r});
         compteur++;
     }
@@ -151,31 +203,17 @@ const ChartJsExamplePage = () => {
             data: {
                 labels: ['Domain1', 'Domain2', 'Domain3', 'Domain4'],
                 datasets: [{
-                    label: 'label1',
+                    label: 'Cue',
                     backgroundColor: 'rgba(255,0,0,0.6)',
                     data: tabCueXYR
-                },
-                {
-                    label: 'label2',
-                    backgroundColor: 'rgba(0,255,0,0.6)',
-                    data: [{
-                        x: 1, 
-                        y: 1,
-                        r: 4,
-                    },
-                    {
-                        x: 1, 
-                        y: 1,
-                        r: 2,
-                    }]
-                },
-            ],
+                }],
             },
             options: Chart.defaults.bubble,
         });
     }, []);
 
     return (
+        <div className="margin">
         <div className="chartjs-example-page">
             <a
                 href="https://www.chartjs.org/docs/latest/charts/bubble.html"
@@ -185,6 +223,7 @@ const ChartJsExamplePage = () => {
                 Sismographe graph with Chart.js
             </a>
             <canvas ref={canvasRef} />
+        </div>
         </div>
     );
 };
